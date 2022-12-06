9372e89faf1eff3014c4687c76679e0e3d75e8f2
ronen11001 <119411886+ronen11001@users.noreply.github.com>
2022-12-06 13:16:03 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onGesture(Gesture.Shake, function () {
+    Randomnumber = randint(0, 10)
+    basic.showNumber(Randomnumber)
+})
+let Randomnumber = 0
+Randomnumber = 0
+basic.showNumber(Randomnumber)
+basic.forever(function () {
+	
+})
